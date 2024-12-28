377686e4af2d668ec675e732bce4bebb8b42c17a
amal <amal.toumi@ensi-uma.tn>
2024-12-28 12:26:12 +0100

    Score Threshold Check

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ user_question = st.text_input("Enter your question:")
 
 # Display the result when the user submits a question
 if user_question:
-    similar_question, answer = find_similar_question(user_question)
+    similar_question, answer= find_similar_question(user_question)
     
     st.write(f"**Your Question:** {user_question}")
     st.write(f"**Most Similar Question:** {similar_question}")
--- main.py
+++ main.py
@@ -4,18 +4,27 @@ from langchain.embeddings import HuggingFaceEmbeddings
 import pandas as pd
 
 data = pd.read_csv("unique_questions.csv", encoding="ISO-8859-1")
+
 def find_similar_question(example_question):
-    model=HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-mpnet-base-v2")
+    model = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-mpnet-base-v2")
     faq_questions = data["Questions"].tolist()
     faq_embeddings = model.embed_documents(faq_questions)
+    
     # Encode the example question
     example_embedding = model.embed_documents([example_question])
     
     # Calculate cosine similarity between the example and all FAQ questions
-    similarities = cosine_similarity(example_embedding, faq_embeddings)
+    similarities = cosine_similarity(example_embedding, faq_embeddings).flatten()
     
     # Find the index of the most similar question
     most_similar_idx = similarities.argmax()
     
-    # Return the most similar question and its answer
-    return faq_questions[most_similar_idx], data['Answers'].iloc[most_similar_idx]
+    # Retrieve the most similar question, its answer, and the similarity score
+    most_similar_question = faq_questions[most_similar_idx]
+    max_similarity_score = similarities[most_similar_idx]
+    if max_similarity_score < 0.7:
+        most_similar_answer = "Sorry, there is no relevant answer in the database."
+    else:
+        most_similar_answer = data['Answers'].iloc[most_similar_idx]
+
+    return most_similar_question, most_similar_answer
